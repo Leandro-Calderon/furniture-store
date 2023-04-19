903a87180ce15dc132381f2114b34587382b156f
Leandro Calderón <mellyleo17@gmail.com>
2023-04-19 00:07:30 -0300

    textos de producto estilizados

--- src/components/productos.tsx
+++ src/components/productos.tsx
@@ -5,71 +5,71 @@ const productos = () => {
     <section className={Styles.productos}>
       <h2>Nuestros Productos</h2>
       <div className={Styles.listadoProductos}>
-        <div>
+        <div className={Styles.producto}>
           <div>
             <img src="src/assets/img/producto1.jpg" alt="imagen producto" />
           </div>
+          <div className={Styles.textoProducto}>
+            <h3>Producto1</h3>
+            <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
+            <p className={Styles.precio}>$200000</p>
+            <a href="#">Agregar al carrito</a>
+          </div>
         </div>
-        <div>
-          <h3>Producto1</h3>
-          <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
-          <p>$200000</p>
-          <a href="#">Agregar al carrito</a>
-        </div>
-        <div>
+        <div className={Styles.producto}>
           <div>
             <img src="src/assets/img/producto2.jpg" alt="imagen producto" />
           </div>
+          <div className={Styles.textoProducto}>
+            <h3>Producto2</h3>
+            <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
+            <p className={Styles.precio}>$200000</p>
+            <a href="#">Agregar al carrito</a>
+          </div>
         </div>
-        <div>
-          <h3>Producto2</h3>
-          <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
-          <p>$200000</p>
-          <a href="#">Agregar al carrito</a>
-        </div>
-        <div>
+        <div className={Styles.producto}>
           <div>
             <img src="src/assets/img/producto3.jpg" alt="imagen producto" />
           </div>
+          <div className={Styles.textoProducto}>
+            <h3>Producto3</h3>
+            <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
+            <p className={Styles.precio}>$200000</p>
+            <a href="#">Agregar al carrito</a>
+          </div>
         </div>
-        <div>
-          <h3>Producto3</h3>
-          <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
-          <p>$200000</p>
-          <a href="#">Agregar al carrito</a>
-        </div>
-        <div>
+        <div className={Styles.producto}>
           <div>
             <img src="src/assets/img/producto4.jpg" alt="imagen producto" />
           </div>
+          <div className={Styles.textoProducto}>
+            <h3>Producto4</h3>
+            <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
+            <p className={Styles.precio}>$200000</p>
+            <a href="#">Agregar al carrito</a>
+          </div>
         </div>
-        <div>
-          <h3>Producto4</h3>
-          <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
-          <p>$200000</p>
-          <a href="#">Agregar al carrito</a>
-        </div>
-        <div>
+        <div className={Styles.producto}>
           <div>
             <img src="src/assets/img/producto5.jpg" alt="imagen producto" />
           </div>
+          <div className={Styles.textoProducto}>
+            <h3>Producto5</h3>
+            <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
+            <p className={Styles.precio}>$200000</p>
+            <a href="#">Agregar al carrito</a>
+          </div>
         </div>
-        <div>
-          <h3>Producto5</h3>
-          <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
-          <p>$200000</p>
-          <a href="#">Agregar al carrito</a>
-        </div>
-        <div>
+        <div className={Styles.producto}>
           <div>
             <img src="src/assets/img/producto6.jpg" alt="imagen producto" />
           </div>
-        </div>
-        <div>
-          <h3>Producto6</h3>
-          <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
-          <p>$200000</p>
-          <a href="#">Agregar al carrito</a>
+          <div className={Styles.textoProducto}>
+            <h3>Producto6</h3>
+            <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
+            <p className={Styles.precio}>$200000</p>
+            <a href="#">Agregar al carrito</a>
+          </div>
         </div>
       </div>
     </section>
