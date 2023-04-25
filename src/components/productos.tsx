e71d923c7814cb9870b44e06edce769a71d2ef0b
Leandro Calderón <mellyleo17@gmail.com>
2023-04-24 22:40:20 -0300

    imagenes de productos acomodadas

--- src/components/productos.tsx
+++ src/components/productos.tsx
@@ -6,12 +6,12 @@ const productos = () => {
       <h2>Nuestros Productos</h2>
       <div className={Styles.listadoProductos}>
         <div className={Styles.producto}>
-          <div>
-            <img src="src/assets/img/producto1.jpg" alt="imagen producto" />
-          </div>
+          <img src="src/assets/img/producto1.jpg" alt="imagen producto" />
           <div className={Styles.textoProducto}>
             <h3>Producto1</h3>
-            <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
+            <p>
+              dfsadfd sfsd fsdfsdafsa dfdsfsdsfaadd ddddddddddddddddddafsadfsdf
+            </p>
             <p className={Styles.precio}>$200000</p>
             <a className="btn" href="#">
               Agregar al carrito
@@ -19,9 +19,7 @@ const productos = () => {
           </div>
         </div>
         <div className={Styles.producto}>
-          <div>
-            <img src="src/assets/img/producto2.jpg" alt="imagen producto" />
-          </div>
+          <img src="src/assets/img/producto2.jpg" alt="imagen producto" />
           <div className={Styles.textoProducto}>
             <h3>Producto2</h3>
             <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
@@ -32,9 +30,7 @@ const productos = () => {
           </div>
         </div>
         <div className={Styles.producto}>
-          <div>
-            <img src="src/assets/img/producto3.jpg" alt="imagen producto" />
-          </div>
+          <img src="src/assets/img/producto3.jpg" alt="imagen producto" />
           <div className={Styles.textoProducto}>
             <h3>Producto3</h3>
             <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
@@ -45,9 +41,7 @@ const productos = () => {
           </div>
         </div>
         <div className={Styles.producto}>
-          <div>
-            <img src="src/assets/img/producto4.jpg" alt="imagen producto" />
-          </div>
+          <img src="src/assets/img/producto4.jpg" alt="imagen producto" />
           <div className={Styles.textoProducto}>
             <h3>Producto4</h3>
             <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
@@ -58,9 +52,7 @@ const productos = () => {
           </div>
         </div>
         <div className={Styles.producto}>
-          <div>
-            <img src="src/assets/img/producto5.jpg" alt="imagen producto" />
-          </div>
+          <img src="src/assets/img/producto5.jpg" alt="imagen producto" />
           <div className={Styles.textoProducto}>
             <h3>Producto5</h3>
             <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
@@ -71,9 +63,7 @@ const productos = () => {
           </div>
         </div>
         <div className={Styles.producto}>
-          <div>
-            <img src="src/assets/img/producto6.jpg" alt="imagen producto" />
-          </div>
+          <img src="src/assets/img/producto6.jpg" alt="imagen producto" />
           <div className={Styles.textoProducto}>
             <h3>Producto6</h3>
             <p>dfsadfdsfsdfsdfsdafsadfdsfsdsfaaddddddddddddddddddddafsadfsdf</p>
